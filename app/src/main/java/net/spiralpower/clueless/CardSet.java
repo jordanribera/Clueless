f76e227e61237af6b985475023d058249c8f5a89
Jordan Ribera <jordanribera@gmail.com>
2014-11-01 16:06:29 -0700

    created CardSet

--- app/src/main/java/net/spiralpower/clueless/CardSet.java
+++ app/src/main/java/net/spiralpower/clueless/CardSet.java
@@ -1,19 +1,65 @@
 package net.spiralpower.clueless;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by jordan on 11/1/14.
  */
 public class CardSet
 {
 
-    private Room[] RoomSet;
-    private Suspect[] SuspectSet;
-    private Weapon[] WeaponSet;
+    private Set<Card> AllCardsSet;
+
+    private Set<Room> RoomsSet;
+    private Set<Suspect> SuspectsSet;
+    private Set<Weapon> WeaponsSet;
+
+    public CardSet()
+    {
+
+        this.AllCardsSet = new HashSet<Card>();
+        this.RoomsSet = new HashSet<Room>();
+        this.SuspectsSet = new HashSet<Suspect>();
+        this.WeaponsSet = new HashSet<Weapon>();
+
+    }
 
     public void addCard(Card newCard)
     {
 
+        AllCardsSet.add(newCard);
+        if(newCard.getType() == Card.ROOM_CARD) RoomsSet.add((Room)newCard);
+        if(newCard.getType() == Card.SUSPECT_CARD) SuspectsSet.add((Suspect)newCard);
+        if(newCard.getType() == Card.WEAPON_CARD) WeaponsSet.add((Weapon)newCard);
 
+    }
+
+    public Set<Card> getCards()
+    {
+
+        return this.AllCardsSet;
+
+    }
+
+    public Set<Room> getRooms()
+    {
+
+        return this.RoomsSet;
+
+    }
+
+    public Set<Suspect> getSuspects()
+    {
+
+        return this.SuspectsSet;
+
+    }
+
+    public Set<Weapon> getWeapons()
+    {
+
+        return this.WeaponsSet;
 
     }
 
--- app/src/main/java/net/spiralpower/clueless/TheGame.java
+++ app/src/main/java/net/spiralpower/clueless/TheGame.java
@@ -3,8 +3,14 @@ package net.spiralpower.clueless;
 /**
  * Created by jordan on 11/1/14.
  */
-public class TheGame {
+public class TheGame
+{
+
     private static TheGame ourInstance = new TheGame();
+
+    private CardSet GameCards;
+
+
 
     public static TheGame getInstance() {
         return ourInstance;
@@ -15,7 +21,40 @@ public class TheGame {
     private TheGame()
     {
 
+        this.GameCards = new CardSet();
+        this.fillCards();
 
+    }
+
+    private void fillCards()
+    {
+
+        //add suspects
+        this.GameCards.addCard(new Suspect("Col. Mustard", "Colonel Mustard", "mustard"));
+        this.GameCards.addCard(new Suspect("Prof. Plum", "Professor Plum", "plum"));
+        this.GameCards.addCard(new Suspect("Mr. Green", "Mister Green", "green"));
+        this.GameCards.addCard(new Suspect("Mrs. Peacock", "Mistress Peacock", "peacock"));
+        this.GameCards.addCard(new Suspect("Ms. Scarlet", "Miss Scarlet", "scarlet"));
+        this.GameCards.addCard(new Suspect("Mrs. White", "Mistress White", "white"));
+
+        //add weapons
+        this.GameCards.addCard(new Weapon("Knife", "A sharp knife.", "knife"));
+        this.GameCards.addCard(new Weapon("Candlestick", "A heavy metal candlestick.", "candlestick"));
+        this.GameCards.addCard(new Weapon("Revolver", "A revolving pistol.", "revolver"));
+        this.GameCards.addCard(new Weapon("Rope", "A sturdy length of rope.", "rope"));
+        this.GameCards.addCard(new Weapon("Lead Pipe", "A thick lead plumbing pipe.", "pipe"));
+        this.GameCards.addCard(new Weapon("Wrench", "A mechanic's wrench.", "wrench"));
+
+        //add rooms
+        this.GameCards.addCard(new Room("Hall", "The hall.", "hall"));
+        this.GameCards.addCard(new Room("Lounge", "The lounge.", "lounge"));
+        this.GameCards.addCard(new Room("Dining Room", "The dining room.", "dining_room"));
+        this.GameCards.addCard(new Room("Kitchen", "The kitchen.", "kitchen"));
+        this.GameCards.addCard(new Room("Ballroom", "The ballroom.", "ballroom"));
+        this.GameCards.addCard(new Room("Conservatory", "The conservatory.", "conservatory"));
+        this.GameCards.addCard(new Room("Billiard Room", "The billiard room.", "billiard_room"));
+        this.GameCards.addCard(new Room("Library", "The library.", "library"));
+        this.GameCards.addCard(new Room("Study", "The study.", "study"));
 
     }
 }
